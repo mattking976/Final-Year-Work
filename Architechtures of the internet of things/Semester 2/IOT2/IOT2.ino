ea5916217b35138df083174e2b18183e3487feb9
Matt <matthew_d_king@outlook.com>
2019-05-21 14:03:33 +0100

    Create IOT2.ino

---
+++ Architechtures of the internet of things/Semester 2/IOT2/IOT2.ino
@@ -0,0 +1,20 @@
+//tempCel = ((temp*5) / 1023.0) * 100.0; formula for lm35 to degC
+//variable declarations
+int LED1 = 2;
+int LED2 = 3;
+int LDR1 = A0;
+int TMP1 = A1;
+int TMP2 = A2;
+float LDR1_val = 0;
+float TMP1_val = 0;
+float TMP2_val = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
